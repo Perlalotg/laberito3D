3415fc1c6a4f3db8bbef0d6a87479eb22b1d688e
Perla N. Lopez <perlalotg@outlook.com>
2017-08-23 20:53:34 -0500

    first commit

---
+++ Assets/scrips/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+  [SerializeField]
+  Camera FrontCamera;
+  [SerializeField]
+  Camera BackCamera;
+  private bool switchCam = false;
+  private bool backCam = false;
+  void Start()
+  {
+    FrontCamera.GetComponent<Camera>().enabled = true;
+    BackCamera.GetComponent<Camera>().enabled = false;
+
+  }
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.M))
+    {
+      switchCam = !switchCam;
+      backCam = false;
+    }
+    if (Input.GetKeyDown(KeyCode.N))
+    {
+      switchCam = false;
+      backCam = true;
+    }
+    if (switchCam)
+    {
+      FrontCamera.GetComponent<Camera>().enabled = true;
+      BackCamera.GetComponent<Camera>().enabled = false;
+    }
+    else if (backCam)
+    {
+      FrontCamera.GetComponent<Camera>().enabled = false;
+      BackCamera.GetComponent<Camera>().enabled = true;
+    }
+  }
+}
