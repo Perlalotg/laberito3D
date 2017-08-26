f17793ef2c00807ff7b1d9339257a72dfb9910f8
Perla N. Lopez <perlalotg@outlook.com>
2017-08-25 20:52:30 -0500

    final

--- Assets/scrips/PlayerController.cs
+++ Assets/scrips/PlayerController.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 [DisallowMultipleComponent]
 public class PlayerController : MonoBehaviour
 
 {
   public float speed;
+  public Text countText;
+  public Text winText;
   private Rigidbody rb;
+  private int count;
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
   }
   void FixedUpdate()
   {
@@ -22,9 +29,17 @@ public class PlayerController : MonoBehaviour
     if (other.gameObject.CompareTag("Pick Up"))
     {
       other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
     }
-
   }
-
+  void SetCountText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 10)
+    {
+      winText.text = "YOU WIN";
+    }
+  }
 
 }
